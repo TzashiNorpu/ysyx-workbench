916b6a815f937fadcc40601504ac5a0941bcfe3f
TzashiNorpu <TzashiNorpu@gmail.com>
2023-07-25 16:46:51 +0800

    lamps

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,37 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <nvboard.h>
+#include "Vtop.h"
+
+
+
+
+static TOP_NAME dut;
+void nvboard_bind_all_pins(Vtop* top);
+#define LOOP 1000000
+static void single_cycle() {
+  dut.clk = 0; dut.eval();
+  dut.clk = 1; dut.eval();
+}
+
+static void reset(int n) {
+  dut.rst = 1;
+  while (n -- > 0) single_cycle();
+  dut.rst = 0;
+}
 
 int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+  nvboard_bind_all_pins(&dut);
+  nvboard_init();
+
+  reset(10);
+  int i=0;
+
+  while( i < LOOP) {
+    nvboard_update();
+    single_cycle();
+    i++;
+  }
+
+  nvboard_quit();
 }
--- npc/vsrc/example.v
+++ npc/vsrc/example.v
@@ -1,2 +1,14 @@
-module example();
+module example(
+  input clk,
+  input rst,
+  output reg [15:0] ledr
+);
+  reg [31:0] count;
+  always @(posedge clk) begin
+    if (rst) begin ledr <= 1; count <= 0; end
+    else begin
+      if (count % 10 == 0) ledr <= {ledr[14:0], ledr[15]};
+      count <= (count >= 500 ? 32'b0 : count + 1);
+    end
+  end
 endmodule
